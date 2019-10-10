4718870207d9e41c63d57781fa607eb198a1f51f
Alexey Babkin <alexey.babkin@symbiont.io>
2019-10-10 17:59:48 -0400

    add evil goggles

--- custom.el
+++ custom.el
@@ -5,10 +5,16 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-replace-with-register quelpa yatemplate which-key smartparens rainbow-mode rainbow-delimiters quelpa-use-package projectile magit helm git-gutter general flycheck flx-ido expand-region evil-commentary doom-themes))))
+    (evil-goggles yatemplate which-key smartparens rainbow-mode rainbow-delimiters quelpa-use-package projectile magit helm git-gutter general flycheck flx-ido expand-region evil-replace-with-register evil-commentary doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
--- init.el
+++ init.el
@@ -169,9 +169,16 @@
   :config
   (evil-mode 1))
 
-(use-package expand-region
-  :commands 'er/expand-region
-  :bind ("C-=" . er/expand-region))
+(use-package evil-goggles
+  :ensure t
+  :config
+  (evil-goggles-mode)
+  ;; optionally use diff-mode's faces; as a result, deleted text
+  ;; will be highlighed with `diff-removed` face which is typically
+  ;; some red color (as defined by the color theme)
+  ;; other faces such as `diff-added` will be used for other actions
+  (evil-goggles-use-diff-faces)
+)
 
 ;; Git
 (use-package git-gutter
